49a514ab62195582bedc7c9ea6b9f1f44caa7b70
BRESNEY DAINOVER QUINTANA LONDO�O <bresneyquintana196602@correo.itm.edu.co>
2019-09-01 10:01:06 -0500

    Add user entities

--- MyLeassing.Web/Data/Entities/Manager.cs
+++ MyLeassing.Web/Data/Entities/Manager.cs
@@ -1,21 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace MyLeassing.Web.Data.Entities
 {
-    public class Owner
+    public class Manager
     {
         public int Id { get; set; }
 
         public User User { get; set; }
-
-        public ICollection<Contract> contracts { get; set; }
-
-        public ICollection<Property> Properties { get; set; }
-
-
     }
 }
--- MyLeassing.Web/Startup.cs
+++ MyLeassing.Web/Startup.cs
@@ -6,11 +6,14 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using MyLeassing.Web.Data;
+using MyLeassing.Web.Data.Entities;
+using MyLeassing.Web.Helpers;
 
 namespace MyLeassing.Web
 {
@@ -23,21 +26,33 @@ namespace MyLeassing.Web
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.Configure<CookiePolicyOptions>(options =>
             {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
+
+            services.AddIdentity<User, IdentityRole>(cfg =>
+            {
+                cfg.User.RequireUniqueEmail = true;
+                cfg.Password.RequireDigit = false;
+                cfg.Password.RequiredUniqueChars = 0;
+                cfg.Password.RequireLowercase = false;
+                cfg.Password.RequireNonAlphanumeric = false;
+                cfg.Password.RequireUppercase = false;
+            })
+                .AddEntityFrameworkStores<DataContext>();
+
 
             services.AddDbContext<DataContext>(cfg =>
             {
                 cfg.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
             });
 
+            services.AddTransient<SeedDb>();
+            services.AddScoped<IUserHelper, UserHelper>();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
@@ -56,6 +71,7 @@ namespace MyLeassing.Web
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+            app.UseAuthentication();
             app.UseCookiePolicy();
 
             app.UseMvc(routes =>
